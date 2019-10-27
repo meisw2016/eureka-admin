ee84b48d3391a68eef262dc848f1cb5c94f39c3f
meisw2016 <meisw_2016@163.com>
2019-10-27 18:46:48 +0800

    修改多数据源

---
+++ multi-datasource-jpa/src/main/java/com/meisw/multiDatasource/repository/primary/UserRepository_1.java
@@ -0,0 +1,14 @@
+package com.meisw.multiDatasource.repository.primary;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.meisw.multiDatasource.entity.primary.User1;
+
+/**
+ * @author Yingyu Li
+ * @create 2018-07-13 10:40
+ **/
+
+public interface UserRepository_1 extends JpaRepository<User1,Integer> {
+
+}
---
+++ multi-datasource-jpa/src/main/java/com/meisw/multiDatasource/repository/secondary/UserRepository_2.java
@@ -0,0 +1,14 @@
+package com.meisw.multiDatasource.repository.secondary;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.meisw.multiDatasource.entity.secondary.User2;
+
+/**
+ * @author Yingyu Li
+ * @create 2018-07-13 10:40
+ **/
+
+public interface UserRepository_2 extends JpaRepository<User2,Integer> {
+
+}
